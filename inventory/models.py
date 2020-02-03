b0927e06aedb2be87ecaf74f738887fac60e4bf7
mridubhatnagar <mridubhatnagar@rediffmail.com>
2020-02-04 01:17:17 +0530

    1. Added gitignore file. 2. Indentation from tabs to spaces. 3. Code optimization.

--- inventory/models.py
+++ inventory/models.py
@@ -4,36 +4,36 @@ from inventory import db
 
 
 class Product(db.Model):
-	id=db.Column(db.Integer(), primary_key=True)
-	name=db.Column(db.String(200), nullable=False, unique=True)
-	product_movement = db.relationship('ProductMovement', backref='product', lazy=True)
+    id=db.Column(db.Integer(), primary_key=True)
+    name=db.Column(db.String(200), nullable=False, unique=True)
+    product_movement = db.relationship('ProductMovement', backref='product', lazy=True)
 
-	def __repr__(self):
-		return "{}".format(self.id)
+    def __repr__(self):
+        return "{}".format(self.id)
 
 class Location(db.Model):
-	id=db.Column(db.Integer(), primary_key=True)
-	name=db.Column(db.String(200), nullable=False, unique=True)
+    id=db.Column(db.Integer(), primary_key=True)
+    name=db.Column(db.String(200), nullable=False, unique=True)
 
-	def __repr__(self):
-		return "{}".format(self.id)
+    def __repr__(self):
+        return "{}".format(self.id)
 
 class ProductMovement(db.Model):
 
-	__tablename__ = 'product_movement'
-	
-	id=db.Column(db.Integer(), primary_key=True)
-	product_id = db.Column(db.Integer(), db.ForeignKey('product.id'), nullable=False)
-	
-	from_location_id = db.Column(db.Integer(), db.ForeignKey('location.id'), nullable=True)
-	to_location_id  = db.Column(db.Integer(), db.ForeignKey('location.id'), nullable=True)
-	
-	from_location = db.relationship('Location', foreign_keys=[from_location_id])
-	to_location = db.relationship('Location', foreign_keys=[to_location_id])
+    __tablename__ = 'product_movement'
+    
+    id=db.Column(db.Integer(), primary_key=True)
+    product_id = db.Column(db.Integer(), db.ForeignKey('product.id'), nullable=False)
+    
+    from_location_id = db.Column(db.Integer(), db.ForeignKey('location.id'), nullable=True)
+    to_location_id  = db.Column(db.Integer(), db.ForeignKey('location.id'), nullable=True)
+    
+    from_location = db.relationship('Location', foreign_keys=[from_location_id])
+    to_location = db.relationship('Location', foreign_keys=[to_location_id])
 
 
-	quantity = db.Column(db.Integer)
+    quantity = db.Column(db.Integer)
 
-	def __repr__(self):
-		return "{}".format(self.id)
+    def __repr__(self):
+        return "{}".format(self.id)
     
