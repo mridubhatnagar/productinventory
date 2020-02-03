eda7d8e6fccac43da44e0cc38e8000c774c0e7a8
mridubhatnagar <mridubhatnagar@rediffmail.com>
2020-02-04 02:15:05 +0530

    indentation fixes

--- inventory/routes.py
+++ inventory/routes.py
@@ -10,119 +10,118 @@ from functools import reduce
 
 @app.route('/about')
 def about():
-	return render_template('home.html')
+    return render_template('home.html')
 
 @app.route('/products')
 def products():
-	products = Product.query.all()
-	if len(products) != 0:
-		return render_template('products.html', title='Product', products=products)
-	else:
-		flash('No Products present in the inventory system. Add Product')
+    products = Product.query.all()
+    if len(products) != 0:
+        return render_template('products.html', title='Product', products=products)
+    else:
+        flash('No Products present in the inventory system. Add Product')
 
 @app.route('/add_product', methods=["POST", "GET"])
 def add_product():
-	form=ProductForm()
-	if request.method=="POST" and form.validate():
-		product_id=Product(name=form.name.data)
-		db.session.add(product_id)
-		db.session.commit()
-		flash('Entered Product ID added!', 'success')
-		return redirect(url_for('products'))
-	return render_template('add_product.html', form=form)
+    form=ProductForm()
+    if request.method=="POST" and form.validate():
+        product_id=Product(name=form.name.data)
+        db.session.add(product_id)
+        db.session.commit()
+        flash('Entered Product ID added!', 'success')
+        return redirect(url_for('products'))
+    return render_template('add_product.html', form=form)
 
 
 @app.route('/edit_product/<string:id>', methods=['GET', 'POST'])
 def edit_product(id):
-	product_name=Product.query.filter_by(id=id).first()
-	form=ProductForm()
-	form.name.data=product_name.name
-	if request.method=="POST" and form.validate():
-		product_name.name=request.form["name"]
-		db.session.commit()
-		flash('Updated Product ID', 'success')
-		return redirect(url_for('products'))
-	return render_template('edit_product.html', form=form)
+    product_name=Product.query.filter_by(id=id).first()
+    form=ProductForm()
+    form.name.data=product_name.name
+    if request.method=="POST" and form.validate():
+        product_name.name=request.form["name"]
+        db.session.commit()
+        flash('Updated Product ID', 'success')
+        return redirect(url_for('products'))
+    return render_template('edit_product.html', form=form)
 
 
 @app.route('/locations')
 def locations():
-	locations = Location.query.all()
-	if len(locations) != 0:
-		return render_template('location.html', title='Location', locations=locations)
-	else:
-		flash('No Location present in the inventory. Add Location')
+    locations = Location.query.all()
+    if len(locations) != 0:
+        return render_template('location.html', title='Location', locations=locations)
+    else:
+        flash('No Location present in the inventory. Add Location')
 
 @app.route('/edit_location/<string:id>', methods=['GET', 'POST'])
 def edit_location(id):
-	form=LocationForm()
-	product_location=Location.query.get(id)
-	form.name.data=product_location.name
-	if request.method=="POST" and form.validate():
-		product_location.name=request.form["name"]
-		db.session.commit()
-		flash('Updated Product Location', 'success')
-		return redirect(url_for('locations'))
-	return render_template('edit_location.html', form=form)	
+    form=LocationForm()
+    product_location=Location.query.get(id)
+    form.name.data=product_location.name
+    if request.method=="POST" and form.validate():
+        product_location.name=request.form["name"]
+        db.session.commit()
+        flash('Updated Product Location', 'success')
+        return redirect(url_for('locations'))
+    return render_template('edit_location.html', form=form) 
 
 @app.route('/add_location', methods=["POST", "GET"])
 def add_location():
-	form=LocationForm()
-	if request.method=="POST" and form.validate():
-		location_id = Location(name=form.name.data)
-		db.session.add(location_id)
-		db.session.commit()
-		flash('Entered Location ID added!', 'success')
-		return redirect(url_for('locations'))
-	return render_template('add_location.html', title='Add Location', form=form)
+    form=LocationForm()
+    if request.method=="POST" and form.validate():
+        location_id = Location(name=form.name.data)
+        db.session.add(location_id)
+        db.session.commit()
+        flash('Entered Location ID added!', 'success')
+        return redirect(url_for('locations'))
+    return render_template('add_location.html', title='Add Location', form=form)
 
 @app.route('/productmovement')
 def product_movement():
-	product_movements = ProductMovement.query.all()
-	if len(product_movements) != 0:
-		return render_template('productmovement.html', title='Product Movement', 
-			product_movements=product_movements)
-	else:
-		flash('No Product Movement has taken place.')
+    product_movements = ProductMovement.query.all()
+    if len(product_movements) != 0:
+        return render_template('productmovement.html', title='Product Movement', 
+            product_movements=product_movements)
+    else:
+        flash('No Product Movement has taken place.')
 
 @app.route('/add_productmovement', methods=["POST", "GET"])
 def add_product_movement():
-	D={}
-	total_product_quantities = {}
-	form=ProductMovementForm()
-	products=Product.query.all()
-	locations = Location.query.all()
-	form.from_location.choices = [(location.id,location.name) for location in locations]
-	form.from_location.choices.append((' ',"--"))
-	form.to_location.choices = [(location.id,location.name) for location in locations]
-	form.to_location.choices.append((' ',"--"))
-	form.product_id.choices = [(product.id,product.name) for product in products]
-	if request.method=="POST" and form.validate():
-		import ipdb; ipdb.set_trace();
-		from_location = Location.query.get(form.from_location.data)
-		to_location = Location.query.get(form.to_location.data)
-		product_id = Product.query.get(form.product_id.data)
-		quantity = form.quantity.data
-		
-		product_movement = ProductMovement.query.filter_by(from_location_id=from_location.id).all()
+    D={}
+    total_product_quantities = {}
+    form=ProductMovementForm()
+    products=Product.query.all()
+    locations = Location.query.all()
+    form.from_location.choices = [(location.id,location.name) for location in locations]
+    form.from_location.choices.append((' ',"--"))
+    form.to_location.choices = [(location.id,location.name) for location in locations]
+    form.to_location.choices.append((' ',"--"))
+    form.product_id.choices = [(product.id,product.name) for product in products]
+    if request.method=="POST" and form.validate():
+        from_location = Location.query.get(form.from_location.data)
+        to_location = Location.query.get(form.to_location.data)
+        product_id = Product.query.get(form.product_id.data)
+        quantity = form.quantity.data
+        
+        product_movement = ProductMovement.query.filter_by(from_location_id=from_location.id).all()
 
-		for movement_details in product_movement:
-			if movement_details.product.name not in D:
-				D.update({movement_details.product.name: [movement_details.quantity]})
-			else:
-				D[movement_details.product.name].append(movement_details.quantity)
+        for movement_details in product_movement:
+            if movement_details.product.name not in D:
+                D.update({movement_details.product.name: [movement_details.quantity]})
+            else:
+                D[movement_details.product.name].append(movement_details.quantity)
 
-		for product_details in D:
-			total_product_quantities[product_details] = {}
-			if len(D[product_details]) > 1:
-				total_product_quantities[product_details] = sum(D[product_details])
-			else:
-				total_product_quantities[product_details] = D[product_details][0]
+        for product_details in D:
+            total_product_quantities[product_details] = {}
+            if len(D[product_details]) > 1:
+                total_product_quantities[product_details] = sum(D[product_details])
+            else:
+                total_product_quantities[product_details] = D[product_details][0]
 
-		if product_id.name in total_product_quantities:
-			if total_product_quantities[product_id.name] < quantity:
-				flash('Not enough stock available at location', 'success')
-				return redirect(url_for('product_movement'))
+        if product_id.name in total_product_quantities:
+            if total_product_quantities[product_id.name] < quantity:
+                flash('Not enough stock available at location', 'success')
+                return redirect(url_for('product_movement'))
             else:       
                 data = ProductMovement(from_location_id=from_location, 
                                to_location_id=to_location,
@@ -132,56 +131,56 @@ def add_product_movement():
                 db.session.commit()
                 flash('Product Movement Details added', 'success')
                 return redirect(url_for('product_movement'))
-	return render_template('add_productmovement.html', title='Add Product Movement', form=form)
+    return render_template('add_productmovement.html', title='Add Product Movement', form=form)
 
 @app.route('/edit_productmovement/<int:id>', methods=['GET', 'POST'])
 def edit_productmovement(id):
-	product_movement=ProductMovement.query.get(id)
-	print(product_movement.from_location.name)
-	form=ProductMovementForm()
-	products=Product.query.all()
-	locations = Location.query.all()
-	form.from_location.choices = [(location.id,location.name) for location in locations]
-	form.from_location.choices.append((' ',"--"))
-	form.to_location.choices = [(location.id,location.name) for location in locations]
-	form.to_location.choices.append((' ',"--"))
-	form.product_id.choices = [(product.id,product.name) for product in products]
-	form.from_location.default=product_movement.from_location.id
-	form.to_location.default=product_movement.to_location.id if product_movement.to_location != None else ' '
-	form.product_id.default=product_movement.product.id 
-	form.quantity.default=product_movement.quantity
-	form.process()
-	
-	if request.method=="POST" and form.validate():
-		product_movement.to_location=Location.query.get(request.form["to_location"])
-		product_movement.from_location=Location.query.get(request.form["from_location"])
-		product_movement.product_id=Product.query.get(request.form["product_id"])
-		product_movement.quantity=request.form["quantity"]
-		db.session.commit()
-		flash('Updated Product Movement', 'success')
-		return redirect(url_for('product_movement'))
-	return render_template('edit_productmovement.html', form=form)	
+    product_movement=ProductMovement.query.get(id)
+    print(product_movement.from_location.name)
+    form=ProductMovementForm()
+    products=Product.query.all()
+    locations = Location.query.all()
+    form.from_location.choices = [(location.id,location.name) for location in locations]
+    form.from_location.choices.append((' ',"--"))
+    form.to_location.choices = [(location.id,location.name) for location in locations]
+    form.to_location.choices.append((' ',"--"))
+    form.product_id.choices = [(product.id,product.name) for product in products]
+    form.from_location.default=product_movement.from_location.id
+    form.to_location.default=product_movement.to_location.id if product_movement.to_location != None else ' '
+    form.product_id.default=product_movement.product.id 
+    form.quantity.default=product_movement.quantity
+    form.process()
+    
+    if request.method=="POST" and form.validate():
+        product_movement.to_location=Location.query.get(request.form["to_location"])
+        product_movement.from_location=Location.query.get(request.form["from_location"])
+        product_movement.product_id=Product.query.get(request.form["product_id"])
+        product_movement.quantity=request.form["quantity"]
+        db.session.commit()
+        flash('Updated Product Movement', 'success')
+        return redirect(url_for('product_movement'))
+    return render_template('edit_productmovement.html', form=form)  
 
 
 
 @app.route('/dashboard')
 def dashboard():
-	D = {}
-	total_product_quantities = {}
-	locations=Location.query.all()
-	for location in locations:
-		D[location.name] = {}  
-		product_locations=ProductMovement.query.filter_by(to_location_id=location.id).all()
-		for product_location in product_locations:
-			if product_location.product.name not in D[location.name]:
-				D[location.name].update({product_location.product.name: [product_location.quantity]})
-			else:
-				D[location.name][product_location.product.name].append(product_location.quantity)
-	for product_location in D:
-		total_product_quantities[product_location] = {}
-		for product_details in D[product_location]:
-			if len(D[product_location][product_details]) > 1:
-				total_product_quantities[product_location][product_details] = sum(D[product_location][product_details])
-			else:
-				total_product_quantities[product_location][product_details] = D[product_location][product_details][0]
-	return render_template('dashboard.html', title='Dashboard', product_details=total_product_quantities)
+    D = {}
+    total_product_quantities = {}
+    locations=Location.query.all()
+    for location in locations:
+        D[location.name] = {}  
+        product_locations=ProductMovement.query.filter_by(to_location_id=location.id).all()
+        for product_location in product_locations:
+            if product_location.product.name not in D[location.name]:
+                D[location.name].update({product_location.product.name: [product_location.quantity]})
+            else:
+                D[location.name][product_location.product.name].append(product_location.quantity)
+    for product_location in D:
+        total_product_quantities[product_location] = {}
+        for product_details in D[product_location]:
+            if len(D[product_location][product_details]) > 1:
+                total_product_quantities[product_location][product_details] = sum(D[product_location][product_details])
+            else:
+                total_product_quantities[product_location][product_details] = D[product_location][product_details][0]
+    return render_template('dashboard.html', title='Dashboard', product_details=total_product_quantities)
